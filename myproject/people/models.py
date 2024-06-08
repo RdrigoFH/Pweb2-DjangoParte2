3ac8d4bb27da48173f412b9d03a819880de48c67
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-08 13:42:15 -0500

    Se documenta cuando un campo es obligatorio y cuando opcional

--- myproject/people/models.py
+++ myproject/people/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=50)  
+    first_name = models.CharField(max_length=50)  #Camapos obligatorios por no indicar si pueden quedar en blanco
     last_name = models.CharField(max_length=50)   
-    email = models.EmailField(max_length=50, blank=True, null=True)  
+    email = models.EmailField(max_length=50, blank=True, null=True)  #Campos opcioneles
     age = models.PositiveIntegerField(blank=True, null=True)  
 
     def __str__(self):
