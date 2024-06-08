1382d43956f07f92d47bfaacd790fa2db7c4d2aa
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-08 13:37:31 -0500

    Se cambia el modelo para hacer opcionales los campos de edad y email

--- myproject/people/models.py
+++ myproject/people/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=50)  
+    last_name = models.CharField(max_length=50)   
+    email = models.EmailField(max_length=50, blank=True, null=True)  
+    age = models.PositiveIntegerField(blank=True, null=True)  
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
