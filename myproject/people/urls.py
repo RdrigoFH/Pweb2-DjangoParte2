a956110aa6ee9e2ce0ba5b6f9c04c0c5227209b6
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-08 14:20:41 -0500

    Se configuraron las urls de people y de myproject para mostrar las vistas

--- myproject/people/urls.py
+++ myproject/people/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), 
-    path('person/<int:id>/', views.PersonDetailView.as_view(), name='person_detail'),  
+    path('', views.index, name='index'),  
+    path('person/<int:id>/', views.PersonDetailView.as_view(), name='person_detail'),
 ]
