d40dd9f96709a5aa905ba683bd917b3271ad5eef
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-08 13:02:07 -0500

    Se agrego el campo email para hacer las pruebas y visualizar que ocurre con los objetos persona anteriormente creados y saber con que nuevo valor se autocompleta el campo email

--- myproject/people/models.py
+++ myproject/people/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
     age = models.PositiveIntegerField()
 
     def __str__(self):
