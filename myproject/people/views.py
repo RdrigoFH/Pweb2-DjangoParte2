b462371f9739f8ab7fc299bc17c58472894ae7ae
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-08 14:06:44 -0500

    Se crea la clase PersonalDataView para tener un mejor manejo de diferentes metodos HTTPS desde view.py

--- myproject/people/views.py
+++ myproject/people/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from .models import Person
 
 def index(request):
-    people = Person.objects.all()  
+    people = Person.objects.all()
     return render(request, 'people/index.html', {'people': people})
+
+class PersonDetailView(DetailView):
+    model = Person
+    template_name = 'people/person_detail.html'
+    context_object_name = 'person'
